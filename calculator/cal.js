847b04e24a55cf2dcc5a8f0a00040ce87fcdbf88
devanshusharm <63507392+devanshusharm@users.noreply.github.com>
2021-06-08 14:51:07 +0530

    assingment

---
+++ calculator/cal.js
@@ -0,0 +1,34 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = ' ';
+
+for( item of buttons){
+    item.addEventListener('click',(e)=>{
+    buttontext = e.target.innerText;
+    console.log(buttontext);
+    if(buttontext == 'x'){
+        buttontext = '*';
+        screenValue += buttontext
+        screen.value = screenValue;
+        
+    }
+    else if(buttontext == 'C'){
+        screenValue= ""
+        screen.value = screenValue;
+
+    }
+    else if(buttontext == '='){
+        screen.value = eval(screenValue)
+
+    }
+    else{
+        screenValue += buttontext
+        screen.value = screenValue;
+    }
+
+
+
+
+})
+
+}
